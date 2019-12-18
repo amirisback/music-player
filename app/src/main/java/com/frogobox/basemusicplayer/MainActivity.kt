7badd04b890da3731270aa1dc9a888074ef13068
amirisback <faisalamircs@gmail.com>
2019-12-18 22:10:57 +0700

    add: testing admob banner

--- app/src/main/java/com/frogobox/basemusicplayer/MainActivity.kt
+++ app/src/main/java/com/frogobox/basemusicplayer/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.frogobox.basemusicplayer
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.frogobox.basemusicplayer.base.admob.BaseAdmobActivity
+import com.frogobox.basemusicplayer.base.ui.BaseActivity
+import kotlinx.android.synthetic.main.ads_phone_tab_special_smart_banner.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseAdmobActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupShowAdsBanner(ads_phone_tab_special_smart_banner)
+
     }
+
+
+
 }
